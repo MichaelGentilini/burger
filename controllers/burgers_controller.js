6629523a752a3be87eefc8601c81bf1e426e82c6
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-06-11 17:55:22 -0500

    eaten burgers moving to the right

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -34,7 +34,6 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
-
   console.log("condition", condition);
 
   burger.updateOne({
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -35,20 +35,46 @@ $(function () {
 
 
   // Put / change burger to eaten
-
   //  ? delete/eat burger
-  $(".delete-burger").on("click", function (event) {
-    var id = $(this).data("id");
+  $(".eat-burger").on("click", function (event) {
+    // console.log(this).data('id');
+    console.log($(this))
 
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+    var newDevoured = $(this).data("newdevoured");
+
+    var eaten = {
+      devoured: newDevoured
+    };
+    var $button = $(this);
+
+    console.log($button)
+    console.log("devoured was set to " + devoured);
+    console.log("devoured is now set to " + newDevoured);
+
+    if ($(this).data("devoured")) {
+      var $button = $(this);
+      // console.log($button)
+
+      console.log('this has been changed');
+
+      // $(this).parent().remove();
+    }
+    // console.log('eating a burger with id of ' + id);
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
+      type: "Put",
+      data: eaten
     }).then(
       function () {
         console.log("deleted burger", id);
         // Reload the page to get the updated list
-        location.reload();
+        // location.reload();
+        $button.removeClass("btn-success").addClass('btn-danger');
       }
     );
+    $button.removeClass("btn btn-sm btn-success eat-burger").addClass('btn btn-danger');
+
   })
 });
