f65027e8cc4ce9cace8ad62d10656742c097798c
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-06-13 01:19:09 -0500

    found error in connecion.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
 var mysql = require("mysql");
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+    host: "localhost",
     port: 3306,
-    user: "sc2ht1yrctuti0us",
-    password: "tef11dweisptei0j",
-    database: "r1j439akps1z7elh"
+    user: "root",
+    password: "password",
+    database: "burgers_db"
   });
 }
 
