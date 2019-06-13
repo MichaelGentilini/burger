78d93b6554a7e1ede86c8e716e9005e2ede8463b
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-06-13 10:15:34 -0500

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,21 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: "root",
     password: "password",
-    database: "burgers_db"
+    database: "burger_db"
   });
 }
 
-connection.connect(function (err) {
+// else {
+//   connection = mysql.createConnection({
+//     host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+//     port: 3306,
+//     user: "sc2ht1yrctuti0us",
+//     password: "tef11dweisptei0j",
+//     database: "r1j439akps1z7elh"
+//   });
+// }
+
+connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
