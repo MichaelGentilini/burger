5e52959c38354bc5b81c1b2d13250019a9cfa443
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-06-23 14:15:26 -0500

    removed db info from connection

--- config/connection.js
+++ config/connection.js
@@ -9,21 +9,10 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     user: "root",
     password: "password",
-    database: "burger_db"
+    database: "burgers_db"
   });
 }
-
-// else {
-//   connection = mysql.createConnection({
-//     host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-//     port: 3306,
-//     user: "sc2ht1yrctuti0us",
-//     password: "tef11dweisptei0j",
-//     database: "r1j439akps1z7elh"
-//   });
-// }
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
